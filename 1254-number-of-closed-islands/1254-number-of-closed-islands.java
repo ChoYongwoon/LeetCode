eaa20d1fd35529dc2442c151c79d75a4ee1f7222
yongwoon Cho <89126793+ChoYongwoon@users.noreply.github.com>
2023-04-06 10:26:49 +0900

    Time: 2 ms (82.36%), Space: 42.8 MB (69.67%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,8 +1,8 @@
 class Solution {
-    public int closedIsland(int[][] grid) {
+    public int closedIsland(int[][] grid) { // checking closed land for all cell with value 0.
         int res = 0;
         
-        for(int i = 0; i < grid.length; i++){
+        for(int i = 0; i < grid.length; i++){ 
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j]==0){
                     res += dfs(grid,i,j);
@@ -12,14 +12,14 @@ class Solution {
         return res;
     }
     
-    public int dfs(int[][] grid, int i, int j){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length){
+    public int dfs(int[][] grid, int i, int j){ // dfs for check closed or not and renew the value in grid.
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length){ // out of range -> 0(means open)
             return 0;
         }
-        if(grid[i][j] == 1){return 1;}
-        int res = 0;
-        grid[i][j] = 1;
-        res = dfs(grid,i-1,j)*dfs(grid,i,j-1)*dfs(grid,i+1,j)*dfs(grid,i,j+1);
+        if(grid[i][j] == 1){return 1;} // water or already visited land return 0.
+        int res = 0; 
+        grid[i][j] = 1; // For checking visited or not
+        res = dfs(grid,i-1,j)*dfs(grid,i,j-1)*dfs(grid,i+1,j)*dfs(grid,i,j+1); // Every boundary is closed -> 1(closed) or 0(open)
         return res;
     }
 }
