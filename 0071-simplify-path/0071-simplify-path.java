ceaebcb1d9ad2bbedfc7668c51eb35687468c5c4
yongwoon Cho <89126793+ChoYongwoon@users.noreply.github.com>
2023-04-12 18:06:20 +0900

    Time: 7 ms (31.53%), Space: 42.5 MB (74.66%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,26 +1,26 @@
 class Solution {
-    public String simplifyPath(String path) {
-        String[] tmp = path.split("/");
-        String res = "";
-        Stack<String> stack = new Stack<>();
-        for(int i = 0; i < tmp.length; i++){
-            if (tmp[i].length() == 0 || tmp[i].compareTo(".") == 0){
+    public String simplifyPath(String path) { 
+        String[] tmp = path.split("/");                                 // first, split the input by '/'
+        String res = "";                                                // String to be retuned.
+        Stack<String> stack = new Stack<>();                            // Using stack to handle ".."
+        for(int i = 0; i < tmp.length; i++){                            
+            if (tmp[i].length() == 0 || tmp[i].equals(".")){            // If a piece contains nothing or just "." pass
                 continue;
             }
-            else if(tmp[i].compareTo("..")==0){
+            else if(tmp[i].equals("..")){                               // If a piece is "..", execute pop() to trace back
                 if(!stack.isEmpty()){
                     stack.pop();
                 }
             }
-            else{
+            else{                                                       // In normal case, push the directory to stack
                 stack.push(tmp[i]);
             }
         }
         
-        while(!stack.isEmpty()){
+        while(!stack.isEmpty()){                                        // Make the result
             res = "/" + stack.pop() + res;
         }
-        if(res.length() == 0){
+        if(res.length() == 0){                                          // If there's nothing to return, return "/"
             res = "/";
         }
         
