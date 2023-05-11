389e85044927891384037d16985c210714004513
yongwoon Cho <89126793+ChoYongwoon@users.noreply.github.com>
2023-05-11 10:37:30 +0900

    Time: 8 ms (26.05%), Space: 41.5 MB (95.29%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -10,19 +10,20 @@ class Solution {
         return recurSolve(0, 0, nums1, nums2, dp);
     }
     
-    private int recurSolve(int i, int j, int[] nums1, int[] nums2, int[][] dp){
-        if(i >= nums1.length){return 0;}
-        if(j >= nums2.length){return 0;}
-        if(dp[i][j] != -1){return dp[i][j];}
+    private int recurSolve(int i, int j, int[] nums1, int[] nums2, int[][] dp){ // method to solve recursively
+        if(i >= nums1.length){return 0;} // out of range -> return 0
+        if(j >= nums2.length){return 0;} // out of range -> return 0
+        if(dp[i][j] != -1){return dp[i][j];} // if there is precalculated value, return that one
         
-        int next_j = j;
-        while(next_j < nums2.length && nums1[i] != nums2[next_j]){next_j ++;}
+        int next_j = j;                  // variable to indicate the index of nums2 contains the value same with nums1[i]
+        while(next_j < nums2.length && nums1[i] != nums2[next_j]){next_j ++;}   // find the index
         
-        if(next_j == nums2.length){
-            dp[i][j] = recurSolve(i+1, j, nums1, nums2, dp);
+        if(next_j == nums2.length){      // if it is not matched
+            dp[i][j] = recurSolve(i+1, j, nums1, nums2, dp);   // skip i, and solve for next i(i+1)
             return dp[i][j];
-        }else{
-            dp[i][j] = Math.max(1 + recurSolve(i+1, next_j+1, nums1, nums2, dp), recurSolve(i+1, j, nums1, nums2, dp));
+        }else{                           // if it is match
+            // return the bigger set (one contains nums1[i], anothoer one not contains nums1[i])
+            dp[i][j] = Math.max(1 + recurSolve(i+1, next_j+1, nums1, nums2, dp), recurSolve(i+1, j, nums1, nums2, dp));     
             return dp[i][j];
         }
         
