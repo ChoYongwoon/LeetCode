68112561138584bcb27ab926b43b00d7791388e6
yongwoon Cho <89126793+ChoYongwoon@users.noreply.github.com>
2023-06-26 22:09:18 +0900

    Time: 26 ms (100.00%), Space: 54.7 MB (91.04%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
@@ -1,30 +1,33 @@
-class Solution {
+class Solution {    // Used priority queue to manage left candidates and right candidates
     public long totalCost(int[] costs, int k, int candidates) {
-       if(2*candidates + k > costs.length){
-            Arrays.sort(costs);
+       if(2*candidates + k > costs.length){     // In this condition, all cost can be checked
+            Arrays.sort(costs);                 // So, just pick the k smallest costs 
             long result = 0;
             for(int i = 0; i < k ;i++){
                 result += costs[i];
             }
             return result;
         }
-        else{
+        else{ // We should check the smallest cost of left candidates and right candidates seperately
+            // Use priority queue to do it fast!
             PriorityQueue<Integer> left = new PriorityQueue<>();
             PriorityQueue<Integer> right = new PriorityQueue<>();
-            for(int i = 0; i < candidates; i++){
+            for(int i = 0; i < candidates; i++){    // Insert initial candidates
                 left.add(costs[i]);
                 right.add(costs[costs.length-1-i]);
             }
-            int left_idx = left.size();
-            int right_idx = costs.length-1-right.size();
-            long result = 0;
+            int left_idx = left.size();             // index of the next candidate in left pq.
+            int right_idx = costs.length-1-right.size(); // index of the next candidate in right pq.
+            long result = 0;    
 
+            // Calculate the result according to problem descriptions.
+            // pq's peek is always the smallest cost.
             for(int i = 0; i < k; i++){
-                if(left.peek() <= right.peek()){
-                    result += left.remove();
-                    left.add(costs[left_idx++]);
-                }else{
-                    result += right.remove();
+                if(left.peek() <= right.peek()){    // If the right's peek is larger
+                    result += left.remove();        // pop the smallest element in left pq,
+                    left.add(costs[left_idx++]);    // insert the new cost in left index, renew the left index
+                }else{   // Resolve symmetrically
+                    result += right.remove();       
                     right.add(costs[right_idx--]);
                 }
 
