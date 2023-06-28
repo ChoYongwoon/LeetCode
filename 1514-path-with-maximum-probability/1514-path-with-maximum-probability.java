1f4455b20970b57dca93132cb5b84d021d245ec1
yongwoon Cho <89126793+ChoYongwoon@users.noreply.github.com>
2023-06-28 14:25:49 +0900

    Time: 790 ms (5.14%), Space: 57.5 MB (16.94%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -0,0 +1,88 @@
+class Solution {
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        Map<Integer, Set<Pair>> graph = buildGraph(n, edges, succProb);
+        return dijkstra(n, start, end, graph);
+    }
+
+    public Map<Integer, Set<Pair>> buildGraph(int n, int[][] edges, double[] succProb){
+        Map<Integer, Set<Pair>> graph = new HashMap<>();
+        for(int i = 0; i < edges.length; i++){
+            int[] edge = edges[i];
+            if(graph.containsKey(edge[0])){
+                graph.get(edge[0]).add(new Pair(edge[1], succProb[i]));
+            }else{
+                Set<Pair> newSet = new HashSet<>();
+                newSet.add(new Pair(edge[1], succProb[i]));
+                graph.put(edge[0], newSet);
+            }
+            if(graph.containsKey(edge[1])){
+                graph.get(edge[1]).add(new Pair(edge[0], succProb[i]));
+            }else{
+                Set<Pair> newSet = new HashSet<>();
+                newSet.add(new Pair(edge[0], succProb[i]));
+                graph.put(edge[1], newSet);
+            }
+        }
+        return graph;
+    }
+
+    public double dijkstra(int n, int start, int end, Map<Integer, Set<Pair>> graph) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>((Pair p1, Pair p2) -> p1.dist < p2.dist ? 1: -1);
+        Pair[] vertices = new Pair[n];
+        for(int i = 0; i < n; i++){
+            vertices[i] = new Pair(i, 0);
+            vertices[i].dist = -1;
+        }
+        vertices[start].dist = 1;
+        pq.offer(vertices[start]);
+
+        while(!pq.isEmpty()){
+            Pair curr = pq.poll();
+            if(curr.vertex == end){
+                return curr.dist;
+            }
+            Set<Pair> adjs = graph.get(curr.vertex);
+            if(adjs == null){
+                return 0;
+            }
+            for(Pair adj : adjs){
+                int adjVertex = adj.vertex;
+                if(curr.dist * adj.prob > vertices[adjVertex].dist){    // relaxation
+                    pq.remove(vertices[adjVertex]);
+                    vertices[adjVertex].dist = curr.dist * adj.prob;
+                    pq.offer(vertices[adjVertex]);
+                }
+            }
+        }
+        return 0;
+    }
+
+    class Pair{
+        int vertex;
+        double prob;
+        double dist;
+        public Pair(int v, double p){
+            vertex = v;
+            prob = p;
+        }
+
+        @Override
+        public int hashCode() {
+            return vertex % 101;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Pair other = (Pair) obj;
+            if (vertex != other.vertex)
+                return false;
+            return true;
+        }
+    }
+}
